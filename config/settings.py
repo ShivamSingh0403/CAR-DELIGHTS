973c37b172018755a8ecceafea88d3f555911128
Shivam kumar Singh <singhshivam72954@gmail.com>
2026-09-10 20:19:08 +0530

    fix: define staticfiles in STORAGES

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
